028d509fec7c12dcf0577162ef1c8ae3640c4c78
Toumai <daniel.salazar.7e7@itb.cat>
2024-01-16 22:20:54 +0100

    app

--- app/src/main/java/com/example/trivialapp/MainActivity.kt
+++ app/src/main/java/com/example/trivialapp/MainActivity.kt
@@ -13,20 +13,24 @@ import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.trivialapp.ui.theme.TrivialAppTheme
 import viewModel.TriviaSettingsViewModel
+import kotlin.reflect.KProperty
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val settingsViewModel by viewModels<TriviaSettingsViewModel>()
         setContent {
-            TrivialAppTheme {
-                // A surface container using the 'background' color from the theme
+            TrivialAppTheme (darkTheme = settingsViewModel.darkMode) {
+
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
